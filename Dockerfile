1bd2331db90704c0f0bdd70f1ac274e3351a582e
contract2024 <wuyanjun.xiaolian.link>
2024-05-08 16:08:48 +0800

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:3.0.4
+FROM ruby:3.2.3
 
 WORKDIR /srv/slate
 
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 # build futures-api/zh
-docker run --rm --name slate -v $(pwd)/build/zh/latest/futures-api:/srv/slate/build -v $(pwd)/futures-api/zh/index.html.md:/srv/slate/source/index.html.md -v $(pwd)/futures-api/zh/includes:/srv/slate/source/includes futuresdocker/slate build
+docker run --rm --name slate -v $(pwd)/build/zh/latest/API-MPC:/srv/slate/build -v $(pwd)/futures-open-api/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/futures-open-api/zh/includes:/srv/slate/source/includes chainupcustody/slate build
 # build futures-api/en
-#docker run --rm --name slate -v $(pwd)/build/en/latest/futures-api:/srv/slate/build -v $(pwd)/futures-api/en/index.html.md:/srv/slate/source/index.html.md -v $(pwd)/futures-api/en/includes:/srv/slate/source/includes futuresdocker/slate build
+#docker run --rm --name slate -v $(pwd)/build/en/latest/futures-api:/srv/slate/build -v $(pwd)/futures-api/en/_index.md.erb.erb:/srv/slate/source/_index.md.erb.erb -v $(pwd)/futures-api/en/includes:/srv/slate/source/includes futuresdocker/slate build
 
 sudo cp $(pwd)/index.html $(pwd)/build
 
@@ -13,3 +13,5 @@ sudo cp $(pwd)/CNAME $(pwd)/build
 
 
 # git subtree push --prefix build origin gh-pages
+
+
